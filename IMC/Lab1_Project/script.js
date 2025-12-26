de251291a6d09bae9137a4798c5c4d83b6cd5da8
tqbinh1268 <tqbinh@gmail.com>
2025-12-26 15:24:54 +0700

    cập nhật animation

--- IMC/Lab1_Project/script.js
+++ IMC/Lab1_Project/script.js
@@ -1,10 +1,7 @@
 // Script cho hiệu ứng fade-in của banner
 document.addEventListener('DOMContentLoaded', function() {
-    const heroTitle = document.querySelector('.hero-title');
     const heroSubtitle = document.querySelector('.hero-subtitle');
-    // Thêm class 'show' để kích hoạt fade-in
-    heroTitle.classList.add('show');
-    // Subtitle sẽ fade-in sau title
+    // Subtitle sẽ fade-in
     setTimeout(() => {
         heroSubtitle.classList.add('show');
     }, 500);
